bb8c92f046e909265e21d53c0fadf37327fb55a8
Elena-1405 <helenofspb@gmail.com>
2024-03-25 11:58:50 +0300

    ASTONReact-1 [chore]: minor fixes acc. to comments

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import css from './footer.module.css';
 
 export default function Footer() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
   return (
     <footer className={css.footer}>
       <div className={css.txt}>
-        <div>All rights reserved © 2024</div>
+        <div>All rights reserved© {currentYear}</div>
       </div>
     </footer>
   );
