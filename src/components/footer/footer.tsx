ff852bda39b0328c052ab9ee7d35482ee6186ecc
Elena-1405 <helenofspb@gmail.com>
2024-03-25 10:07:34 +0300

    ASTONReact-1 [chore/fix/feat]: components created

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={css.footer}>
+      <div className={css.txt}>
+        <div>All rights reserved © 2024</div>
+      </div>
+    </footer>
+  );
+}
