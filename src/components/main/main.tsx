f46050c375d524860cc7f97b93b0ebc31b205e5f
Elena-1405 <helenofspb@gmail.com>
2024-03-25 12:01:42 +0300

    ASTONReact-1 [chore]: fixes acc.to comments

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import css from './main.module.css';
 import { Search } from '../search/search';
-//import img from '../shared/img.jpg';
 
 export default function Main() {
   return (
-    <main className={css.main}>
+    <div className={css.main}>
       <Search />
       <div className={css.txt}>
         <h3>Добро пожаловать!</h3>
       </div>
 
       <img className={css.img} src="" alt="pic" />
-    </main>
+    </div>
   );
 }
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -4,21 +4,12 @@ import search from '../../shared/search.svg';
 import clear from '../../shared/clear.svg';
 
 export const Search = () => {
-  //const { searchValue, setSearchValue } = useContext(SearchContext)
-
   return (
     <div className={css.search}>
       <img className={css.icon} src={search} alt="search" />
-      <input
-        //value={searchValue}
-        //onChange={(event) => setSearchValue(event.target.value)}
-        className={css.input}
-        placeholder="Поиск"
-      />
-      {
-        //searchValue &&
-        <img className={css.clearIcon} src={clear} alt="clear" />
-      }
+      <input className={css.input} placeholder="Поиск" />
+
+      <img className={css.clearIcon} src={clear} alt="clear" />
     </div>
   );
 };
