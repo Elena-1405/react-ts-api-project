f0d981d82f64bb5104068e84f019ad9a236bcadc
Elena-1405 <helenofspb@gmail.com>
2024-04-11 09:13:07 +0300

    ASTONReact-1 [fix]: search taken from Dmitry's file

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -6,22 +6,50 @@ import { useState, useEffect } from 'react';
 import MovieDataBase from './searchByKeyword';
 
 export const Search = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState<string>('');
   const handleInputChange = (event: { target: HTMLInputElement }) => {
     setInputValue(event.target.value);
   };
 
+  let historySearch: Array<string> = [];
+
+  async function btnSearch(): Promise<void> {
+    const splitHistory = localStorage.getItem('history')?.split(',') || [];
+    const historyExist = splitHistory.filter((searchTerm) => searchTerm === inputValue);
+    if (historyExist.length === 0) {
+      historySearch = splitHistory;
+
+      historySearch.push(inputValue);
+      localStorage.setItem('history', historySearch.toString());
+    }
+
+    console.log(historySearch);
+  }
+  function fan() {
+    if (historySearch?.length === 0) {
+      return <div></div>;
+    }
+    return (
+      <div>
+        {historySearch?.map((elem) => (
+          <ul key={elem}>
+            <li>{elem}</li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className={css.search}>
-      <img className={css.searchIcon} src={search} alt="search" />
       <input
         className={css.input}
         value={inputValue}
         onChange={handleInputChange}
+        onFocus={fan}
         placeholder="Поиск"
       />
-      <img className={css.searchIcon} src={search} alt="search" />
-      <input className={css.input} placeholder="Поиск" />
+      <img className={css.searchIcon} src={search} onClick={btnSearch} alt="search" />
       <img className={css.clearIcon} src={clear} alt="clear" />
       {MovieDataBase(inputValue.trim().replace(/\s+/g, ' ').split(' ').join('%20'))}
     </div>
